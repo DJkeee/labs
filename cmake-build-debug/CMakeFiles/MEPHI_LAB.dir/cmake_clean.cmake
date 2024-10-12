f4cf3d620d3878814623a0862bb3bbd3e878948b
DJkee <olgashutova80@gmail.com>
2024-10-12 20:01:27 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/MEPHI_LAB.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MEPHI_LAB.dir/main.cpp.obj"
+  "CMakeFiles/MEPHI_LAB.dir/main.cpp.obj.d"
+  "MEPHI_LAB.exe"
+  "MEPHI_LAB.exe.manifest"
+  "MEPHI_LAB.pdb"
+  "libMEPHI_LAB.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MEPHI_LAB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
