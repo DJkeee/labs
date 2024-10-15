0a263f93ef17a9606138faaebaed73d4fcbcc6f9
MEPHI_LABS <olgashutova80@gmail.com>
2024-10-15 23:26:59 +0300

    оптимизировал LinkedList , дописал сравнение времени работы с std

--- TimeComparator/TimeComparator.cpp
+++ TimeComparator/TimeComparator.cpp
@@ -1,14 +1,9 @@
-//
-// Created by dmitry on 10/15/24.
-//
-
 #include <iostream>
 #include <vector>
 #include <list>
 #include <chrono>
 #include <random>
-#include "../Sequence/ArraySequence.h"
-#include "../Sequence/ListSequence.h"
+#include "Mainwindow.h"
 
 class TimeComparator {
 public:
@@ -55,25 +50,80 @@ public:
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> distrib(0, 100);
 
-        // Измерение времени для std::list
-        auto start = std::chrono::high_resolution_clock::now();
-        std::list<int> list;
-        for (int i = 0; i < 100000; ++i) {
-            list.push_back(distrib(gen));
-        }
-        auto end = std::chrono::high_resolution_clock::now();
-        auto elapsed_list = timeSpent(start, end);
-        out(elapsed_list, "std::list");
+        // Данные для графика
+        QVector<QPointF> listData, arrayListData;
 
-        // Измерение времени для ArrayList
-        start = std::chrono::high_resolution_clock::now();
-        ListSequence<int> arrayList;
-        for (int i = 0; i < 100000; ++i) {
-            arrayList.append(distrib(gen));
+        for (int n = 1000; n <= 100000; n += 1000) {
+            // Измерение времени для std::list
+            auto start = std::chrono::high_resolution_clock::now();
+            std::list<int> list;
+            for (int i = 0; i < n; ++i) {
+                list.push_back(distrib(gen));
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed_list = timeSpent(start, end);
+            listData << QPointF(n, elapsed_list.count());
+
+            // Измерение времени для ArrayList
+            start = std::chrono::high_resolution_clock::now();
+            ListSequence<int> arrayList;
+            for (int i = 0; i < n; ++i) {
+                arrayList.append(distrib(gen));
+            }
+            end = std::chrono::high_resolution_clock::now();
+            auto elapsed_arrayList = timeSpent(start, end);
+            arrayListData << QPointF(n, elapsed_arrayList.count());
         }
-        end = std::chrono::high_resolution_clock::now();
-        auto elapsed_arrayList = timeSpent(start, end);
-        out(elapsed_arrayList, "ArrayList");
+
+        // Создание графика
+        QChart *chart = new QChart;
+        chart->setTitle("Сравнение времени работы std::list и ArrayList");
+
+        // Линии для std::list и ArrayList
+        QLineSeries *listSeries = new QLineSeries;
+        listSeries->setName("std::list");
+        listSeries->append(listData);
+
+        QLineSeries *arrayListSeries = new QLineSeries;
+        arrayListSeries->setName("ArrayList");
+        arrayListSeries->append(arrayListData);
+
+        // Добавление линий на график
+        chart->addSeries(listSeries);
+        chart->addSeries(arrayListSeries);
+
+        // Оси графика
+        QValueAxis *xAxis = new QValueAxis;
+        xAxis->setTitleText("Количество объектов");
+        xAxis->setRange(0, 100000);
+        chart->addAxis(xAxis, Qt::AlignBottom);
+        listSeries->attachAxis(xAxis);
+        arrayListSeries->attachAxis(xAxis);
+
+        QValueAxis *yAxis = new QValueAxis;
+        yAxis->setTitleText("Время (мс)");
+        yAxis->setRange(0, 10000); // Настройте диапазон по необходимости
+        chart->addAxis(yAxis, Qt::AlignLeft);
+        listSeries->attachAxis(yAxis);
+        arrayListSeries->attachAxis(yAxis);
+
+        // Создание представления графика
+        QChartView *chartView = new QChartView(chart);
+        chartView->setRenderHint(QPainter::Antialiasing);
+
+        // Запуск Qt-приложения
+        QApplication app(argc, argv);
+        chartView->show();
+        return app.exec();
     }
 };
+
+int main(int argc, char *argv[]) {
+    TimeComparator test;
+    //test.compareVectorAndArraySeq();
+    //test.compareListAndArrayList(); // Вызывает Qt приложение для графика
+
+    return 0;
+}
+
 
